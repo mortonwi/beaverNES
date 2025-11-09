d76c35a47c75eefff419c04f83214bc3e59155c2
mortonwi <mortonwi@oregonstate.edu>
2025-11-09 09:23:51 -0800

    main.c creates simple window using SDL2

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <SDL.h>
 
-int main() {
-    printf("Hello World!\n");
+int main(int argc, char *argv[]) {
+    int running = 1;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "beaverNES",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    while(running) {
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
