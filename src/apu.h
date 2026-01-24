7db21a0aa7eff7c6c4403826c3807f5a0e46464d
mortonwi <mortonwi@oregonstate.edu>
2026-01-23 19:33:16 -0800

    Defined structs for APU module

--- src/apu.h
+++ src/apu.h
@@ -1,45 +1,89 @@
 #ifndef APU_H
 #define APU_H
 
-#include <cstdint>
+#include <stdint.h>
 
-/*
- * Contains:
- *      Envelope Generator
- *      Sweep Unit (ignored for now)
- *      Timer
- *      8-Step Sequencer
- *      Length Counter
+/**
+ * @brief Pulse Wave Channel
+ * 
+ * Square wave audio generator.
  */
 typedef struct {
-    uint8_t id;                 // pulsewave 1 or 2
-    uint8_t duty;               // selects pulse wave type
-    uint8_t length;             // length of audio (frozen if looping)
-    uint8_t envelope;           // decreasing saw envelope creates a decay effect
-    uint8_t constant_volume;
-    uint8_t volume;
-    
-    uint16_t timer;             // 11-bit timer controlling pitch
-    uint16_t timer_counter;
+    uint8_t id;                    // pulse 1 or 2
 
-    uint8_t enabled;            // silence / unsilence channel
-    uint8_t phase;              // current position in the waveform
+    uint8_t duty_cycle;            // pulse wave duty (0–3)
+    uint8_t length_halt;           // length counter halt / envelope loop
+    uint8_t constant_volume;       // use constant volume instead of envelope
+    uint8_t envelope_volume;       // envelope volume or decay level
+
+    uint8_t sweep_enabled;         // sweep unit enabled
+    uint8_t sweep_period;          // sweep period
+    uint8_t sweep_negate;          // sweep negate flag
+    uint8_t sweep_shift;           // sweep shift count
+
+    // Timer
+    uint16_t timer_reload_low;     // timer reload low bits
+    uint16_t timer_reload_high;    // timer reload high bits
+    uint16_t length_counter;       // length counter value
 } Pulse;
 
+/**
+ * @brief Triangle Wave Channel
+ * 
+ * Generated audio waves are triangular
+ */
 typedef struct {
-    
+    uint8_t length_halt;            // length counter halt / linear counter control
+    uint16_t linear_reload;         // linear counter reload value
+
+    // Timer
+    uint16_t timer_reload_low;      // timer reload low bits
+    uint16_t timer_reload_high;     // limer reload high bits
+    uint16_t length_counter;        // length counter value
 } Triangle;
 
+/**
+ * @brief Noise Channel
+ * 
+ * Semi-random wave generation resulting in noise.
+ */
 typedef struct {
-    
+    uint8_t length_halt;            // length counter halt / envelope loop
+    uint8_t constant_volume;        // use constant volume
+    uint8_t envelope_volume;        // envelope volume or decay level
+
+    uint8_t lfsr_mode;              // noise mode short / long LFSR
+    uint8_t noise_period;           // noise timer period
+    uint16_t length_counter;        // length counter value
 } Noise;
 
+/**
+ * @brief Delta Modulation Channel
+ *
+ * Allows audio recordings to be played from ROMs on the NES.
+ */
 typedef struct {
-    
-} DMC;
+    uint8_t irq_enable;             // IRQ enable flag
+    uint8_t loop_enabled;           // loop sample playback
+    uint8_t playback_rate;          // DMC playback frequency
 
+    uint16_t sample_counter;        // current sample counter
+
+    uint16_t sample_address;        // starting sample address in memory
+    uint16_t sample_length;         // sample length in bytes
+} Delta;
+
+/**
+ * @brief Audio Processing Unit
+ * 
+ * Main structure for the audio processing unit. Includes all audio channels.
+ */
 typedef struct {
-
+    Pulse pulse1;
+    Pulse pulse2;
+    Triangle triangle;
+    Noise noise;
+    Delta delta;
 } APU;
 
 #endif
